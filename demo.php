5cadc11c2b1adbdde2c6d45f4421804033318c0d
shiva <udayveerdiwake2@r@gmailcom>
2025-04-03 18:35:11 +0530

    sun

--- demo.php
+++ demo.php
@@ -17,8 +17,14 @@ if (!empty($city)) {
     if ($resp) {
         $temp = $data["main"]["temp"];
         $des = $data["weather"][0]["description"];
+        $wind = $data["wind"]["speed"];
+        $humidity = $data["main"]["humidity"];
+        $timezone =  $data["timezone"];
+        $country = $data["sys"]["country"];
+        $sunrise = $data["sys"]["sunrise"];
+        $sunset = $data["sys"]["sunset"];
     } else {
-        $temp = "<div class='alert alert-danger' role='alert'>Weather data in no availble for <b>$city</b></div>";
+        $temp = "<div class='alert alert-danger' role='alert'>Weather data is not availble for <b>$city</b></div>";
     }
 
 
@@ -27,20 +33,21 @@ if (!empty($city)) {
         $email = $_POST['email'];
         $city = $_POST['city'];
 
-        $sql = "INSERT INTO `demo`(`name`, `email`, `city`, `temp`) VALUES ('$name', '$email','$city','$temp')";
+        $sql = "INSERT INTO `demo`(`name`, `email`, `city`, `temp`, `desc`, `wind`, `humidity`) VALUES ('$name', '$email','$city','$temp', '$des', '$wind', '$humidity')";
         $check =  mysqli_query($conn, $sql);
 
         if ($check) {
             $message = "<p class='text-capitalize'>thank you <b>Mr. $name </b> and this is your city <b>$city</b></p>";
         } else {
-            $message = "<div class='alert alert-danger'>City and Name not available</p>";
+            $message = "<div class='alert alert-danger'>Name and City not available</p>";
         }
     }
 } else {
-    echo '<div class="alert alert-danger" role="alert">
+    $temp = '<div class="alert alert-danger" role="alert">
             City is not available
             </div>';
 }
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -52,13 +59,13 @@ if (!empty($city)) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
         body {
-            background-color: rgb(218, 219, 219);
+            background-color: rgb(255, 255, 255);
         }
 
         .container {
             max-width: 500px;
             border-radius: 10px;
-            box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
+            box-shadow: 0px 0px 10px rgb(0, 0, 0);
         }
 
         .container>form>input>.form-control {
@@ -91,9 +98,21 @@ if (!empty($city)) {
         </form>
         <p class="text-center mt-4 text-capitalize fs-4"><strong>
                 <?php
-                echo "Weather in your city <b>$city </b> .<br>";
-                echo "<p>🌡️ Tamprature : <b>$temp</b></p>";
-                echo "<p> description : <b>$des</b></p>";
+                echo "Weather in your city <b>$city </b>  .<br>";
+                echo "<p>🌡️  Tamprature : <b>$temp ℃</b> </p>";
+                echo "<p>💨 Speed  : <b>$wind M/S</b></p>";
+                echo "<p>💧  Humidity  : <b>$humidity%</b></p>";
+                echo "<p>🌤️ Description : <b>$des</b></p>";
+                echo "<p>Country : <b>$country</b></p>";
+                echo "<p>Timezone : <b>" . date("h:i:s A", $timezone) . "</b></p>";
+                echo "<p>Sunrise Time : <b>" . date("d-m-Y H:i:s", $sunrise) . "</b></p>";
+
+                echo ("<br>Sunrise time: ");
+                echo (date_sunrise(time(), SUNFUNCS_RET_STRING, $sunset));
+                echo "<p>Sunset Time : <b>" . date("d-m-Y H:i:s", $sunset) . "</b></p>";
+                // echo "Sunrise Time: " . date("Y-m-d H:i:s", $sunrise) . "\n";
+                // Converts the timestamp to a human-readable format
+                // echo "Sunset Time: " . date("Y-m-d H:i:s", $sunset) . "\n";
                 ?>
             </strong></p>
     </div>
